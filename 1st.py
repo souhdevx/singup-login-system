9f19b393b84cac71a7326c840c4b7d64b3f48168
suuuuhl7 <ssaboua25@gmail.com>
2025-10-21 02:24:58 +0100

    Refactor user management with input validation
    Updated user management functions to improve input validation and prevent duplicate usernames. Enhanced user feedback for signup, login, change password, and account deletion processes.

--- 1st.py
+++ 1st.py
@@ -1,91 +1,120 @@
 import random
 users = []
-def singup():
-    name1 = input("Username: ")
-    password1 = input("Password: ")
+
+def signup():
+    name1 = input("Username: ").strip()
+    if not name1:
+        print("Username cannot be empty.")
+        return
+    # prevent duplicate usernames
+    for u in users:
+        if u["name"] == name1:
+            print("Username already exists.")
+            return
+    password1 = input("Password: ").strip()
+    if not password1:
+        print("Password cannot be empty.")
+        return
     user = {
         "name": name1,
         "password": password1,
     }
     users.append(user)
+    print("Signup successful.")
 
 def login():
-    find = False
-    name2 =  input("Username: ")
-    password2 = input("Password: ")
+    name2 = input("Username: ").strip()
+    password2 = input("Password: ").strip()
     for user in users:
-        if name2 == user["name"] or password2 == user["password"]:
-            find = True
-            print("Welcome back sir! ")
-    if find == False:
-        print("Incorrect username or password")    
+        # require both username and password to match
+        if name2 == user["name"] and password2 == user["password"]:
+            print("Welcome back!")
+            return
+    print("Incorrect username or password.")
+
 def change_password():
-    find = False
-    name = input("Enter you account username: ")
+    name = input("Enter your account username: ").strip()
     for user in users:
         if user["name"] == name:
-            find = True
-            password = input("Enter the curent password: ")
-            if user["password"] == password and user["name"] == name:
-                new_password = input("Enter your new password: ")
-                user["password"] = new_password         
-    if find == False:
-        print("We couldn't find your account")
+            current = input("Enter the current password: ").strip()
+            if user["password"] == current:
+                new_password = input("Enter your new password: ").strip()
+                if new_password:
+                    user["password"] = new_password
+                    print("Password updated successfully.")
+                else:
+                    print("New password cannot be empty.")
+            else:
+                print("Incorrect current password.")
+            return
+    print("We couldn't find your account.")
+
 def delete_account():
-    name3 = input("Enter your username: ")
-    for user in users:  
+    name3 = input("Enter your username: ").strip()
+    for user in users:
         if name3 == user["name"]:
-            print("if you dleted your account you can't login again to your account or recover it!!!!")
-            condition = input("are you shur you want to delete your account(yes/no): ")
+            print("If you delete your account you won't be able to recover it.")
+            condition = input("Are you sure you want to delete your account? (yes/no): ").strip().lower()
             if condition == "yes":
-                    password3 = input("Enter your account password: ")       
-                    if password3 == user["password"] and name3 == user["name"]:
-                        users.remove(user)
-                        print("Your account was deleted successfully")
-                    else:
-                        print("Incorrect password! ")    
+                password3 = input("Enter your account password: ").strip()
+                if password3 == user["password"]:
+                    users.remove(user)
+                    print("Your account was deleted successfully.")
+                else:
+                    print("Incorrect password.")
             elif condition == "no":
-                print("You're welcome any time sir :)")
-        else:
-            print("We couldn't find your account! ")              
-#general programme
+                print("Account not deleted.")
+            else:
+                print("Cancelled.")
+            return
+    print("We couldn't find your account.")
+
+# general program
 def menu():
     print("-----------------------------------------")
-    print("1-signup:")
-    print("2-login")
-    print("3-settings")
-    print("0-close")
+    print("1 - signup")
+    print("2 - login")
+    print("3 - settings")
+    print("0 - close")
     print("-----------------------------------------")
+
 def settings():
     print("-----------------------------------------")
-    print("1-change password")
-    print("2-delete account")
-    print("3-go back")
-    print("-----------------------------------------")    
+    print("1 - change password")
+    print("2 - delete account")
+    print("3 - go back")
+    print("-----------------------------------------")
+
 while True:
     menu()
-    chose = int(input("Take a service: "))
+    try:
+        chose = int(input("Take a service: ").strip())
+    except ValueError:
+        print("Please enter a number.")
+        continue
+
     if chose == 1:
-        singup()
+        signup()
     elif chose == 2:
         login()
     elif chose == 3:
         while True:
             settings()
-            choice = int(input("How we can help you?: "))
+            try:
+                choice = int(input("How can we help you?: ").strip())
+            except ValueError:
+                print("Please enter a number.")
+                continue
             if choice == 1:
                 change_password()
             elif choice == 2:
                 delete_account()
             elif choice == 3:
-                menu()
-                break    
+                break
             else:
-                print("Invaliable service!")
-     
+                print("Invalid service.")
     elif chose == 0:
-        print("see you :)")
-        break    
+        print("See you :)")
+        break
     else:
-        print("this service is invaliable")        
-            
+        print("This service is unavailable.")
